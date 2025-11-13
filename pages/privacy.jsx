66af24629e6ea0766d3b2ce01b91b58942412615
batinowski <ahmetm.demir7@gmail.com>
2025-11-13 13:14:06 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,22 +2,20 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for MusicMaker</h1>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Velocity</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the MusicMaker bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ Velocity Client. <a className="text-white">(BİR HİLEDEN DAHA FAZLASI!)</a></p>
+
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ Lisansını Asla Paylaşamazsın</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the MusicMaker bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Stok Gelmeden Satın Alınan Ürünler Bekletilebilir.</p>   
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using MusicMaker.</p>   
-        <div className="py-3"></div>
-        <p>⊳ MusicMaker bot does not save any data based on users.</p>
-        <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+        <p>⊳ Velocity Kullanıp Satış Yapmak Yasak..</p>
     
         </div>
             </div>
     );
 };
+
