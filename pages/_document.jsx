9a31cb9fcaec438521175d651f00b533bfa503f9
batinowski <ahmetm.demir7@gmail.com>
2025-11-13 13:26:36 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,7 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-
   render() {
     return (
       <Html>
@@ -9,31 +8,18 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/logo.jpg" />
           <meta name="robots" content="index,follow" />
           <meta name="googlebot" content="index,follow" />
-          <meta
-            name="description"
-            content="Bir Hileden Daha Fazlası."
-          />
+          <meta name="description" content="Bir Hileden Daha Fazlası." />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@ayikamann" /
+          <meta name="twitter:site" content="@ayikamann" />
           <meta property="og:url" content="https://discord.gg/5gejGbq5bC" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Velpcity" />
-          <link
-            rel="icon"
-            href="/img/logo2.png"
-            type="image/x-icon"
-          />
-          <meta
-            property="og:description"
-            content="Bir Hileden Daha Fazlası."
-          />
+          <meta property="og:title" content="Velocity" />
+          <link rel="icon" href="/img/logo2.png" type="image/x-icon" />
+          <meta property="og:description" content="Bir Hileden Daha Fazlası." />
           <meta property="og:image" content="/img/logo.jpg" />
           <meta property="og:image:alt" content="Velocity" />
           <meta property="og:locale" content="en_GB" />
-          <meta
-            property="og:site_name"
-            content="Velocity"
-          />
+          <meta property="og:site_name" content="Velocity" />
           <meta name="theme-color" content="#087ddd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
           <link
@@ -45,7 +31,6 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="stylesheet" href="/css/nprogress.css" />
-    
         </Head>
         <body>
           <Main />
