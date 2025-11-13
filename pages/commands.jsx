b1ae7da526dbff04b0e784a41801c54e05fdfbed
batinowski <ahmetm.demir7@gmail.com>
2025-11-13 13:04:56 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -3,32 +3,27 @@ import Link from "next/link";
 
 export default function Commands() {
 const commands = [
-  "/autoplay",
-  "/back",
-  "/statistic",
-  "/channel",
-  "/clear",
-  "/dj",
-  "/filter",
-  "/help",
-  "/language",
-  "/loop",
-  "/nowplaying",
-  "/pause",
-  "/ping",
-  "/play",
-  "/playlist",
-  "/queue",
-  "/resume",
-  "/save",
-  "/search",
-  "/seek",
-  "/servers",
-  "/shuffle",
-  "/skip",
-  "/stop",
-  "/time",
-  "/volume"
+  "KillAura",
+  "Reach",
+  "AimBot",
+  "AntiBot",
+  "İnventory Manager",
+  "Scaffold",
+  "Speed Bridge",
+  "Eagle",
+  "TpAura",
+  "Tp Down",
+  "Tp Up",
+  "Fast Block",
+  "Left Clicker",
+  "Right Clicker",
+  "Custom Hud",
+  "Custom Tab",
+  "Random Name",
+  "Skin Changer",
+  "Fly",
+  "Custom Design",
+  "Config System",
 ];
 
   return (
@@ -39,10 +34,10 @@ const commands = [
       </div>
         <p className="text-xl font-medium text-white">
           <i className="fal fa-cogs text-amber-400 mr-2" />
-         Bot Commands
+         Hile Özellikleri
         </p>
         <p className="text-white text-sm text-opacity-50 mb-5">
-        You can get information about the commands of the MusicMaker Bot.
+        Velocity Client'teki  Özelliklere Burdan Bakabilirsiniz.
         </p>
 
         <div>
@@ -53,7 +48,7 @@ const commands = [
                  
                     <>
                       <i className="fa fa-hashtag text-white text-opacity-20" />
-                     View Commands List
+                     Özellikleri İncele.
                     </>
                 
                 </div>
@@ -89,23 +84,23 @@ const commands = [
 
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-  <Link href={"https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot"}>
+  <Link href={"https://youtube.com"}>
   <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Invite MusicMaker</a>
+Youtube Kanalı</a>
   </Link>
 
   <div className="py-10"></div>
   
-  <Link href={"https://discord.gg/ACJQzJuckW"}>
+  <Link href={"https://discord.gg/5gejGbq5bC"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Support Server</a>
+Satın Alım</a>
   </Link>
 
   <div className="py-10"></div>
   
-  <Link href={"https://top.gg/bot/774043716797071371/vote"}>
+  <Link href={"ayikamann"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote MusicMaker</a>
+Founder Discord</a>
   </Link>
 </div>
 
@@ -114,3 +109,4 @@ Vote MusicMaker</a>
     </>
   )
   }
+
